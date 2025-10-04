260513725da8a7cbe9998e12de7f137f40e169b2
DhineshDhino <k.dhineshasan@gmail.com>
2025-10-05 00:25:59 +0530

    refactor: update component naming to follow Angular conventions and fix file structure

--- What2Cook/src/app/app.component.spec.ts
+++ What2Cook/src/app/app.component.spec.ts
@@ -1,29 +1,23 @@
 import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { App } from './app';
 
-describe('AppComponent', () => {
+describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [App],
     }).compileComponents();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(App);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'What2Cook' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('What2Cook');
-  });
-
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, What2Cook');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, what-2-cook');
   });
 });
--- What2Cook/src/app/app.component.ts
+++ What2Cook/src/app/app.component.ts
@@ -1,13 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'What2Cook';
+  protected readonly title = signal('what-2-cook');
 }
